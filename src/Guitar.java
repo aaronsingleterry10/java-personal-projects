b3ba5ac40918d02b6878acb95e9ec3267dd69255
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-25 14:48:05 -0500

    added constructor/getters/setters for Guitar class

--- src/Guitar.java
+++ src/Guitar.java
@@ -1,5 +1,36 @@
 abstract class Guitar {
     private String brand;
+    private String model;
     private String type;
+
+    public Guitar(String brand, String model, String type) {
+        this.brand = brand;
+        this.model = model;
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 }
