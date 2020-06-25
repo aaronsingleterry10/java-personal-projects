625d288be039c7d6f03103e444845997b464435e
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-25 15:04:14 -0500

    added constructor and implemented methods in FenderTelecaster class

--- src/FenderTelecaster.java
+++ src/FenderTelecaster.java
@@ -1,3 +1,13 @@
 public class FenderTelecaster extends Guitar implements Electric {
+    public FenderTelecaster(String brand, String model, String type) {
+        super(brand, model, type);
+    }
 
+    public void pluggingGuitarCable() {
+        System.out.println("Taking out Mogami cable from bag, uncoiling it and plugging it to guitar...");
+    }
+
+    public void pluggingToAmp() {
+        System.out.println("Plugging other end of cable to Mesa Boogie...");
+    }
 }
