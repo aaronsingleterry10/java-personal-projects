904e642f0f9562f0ba9f35e2a9de455b2e31d3d8
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-25 17:44:03 -0500

    completed practice

--- src/PlayingGuitar.java
+++ src/PlayingGuitar.java
@@ -1,5 +1,11 @@
 public class PlayingGuitar {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Electric guitar1 = new MyMainGuitar("Fender", "Telecaster", "Electric");
+        guitar1.pluggingGuitarCable();
+        guitar1.pluggingToAmp();
+
+        Electric guitar2 = new MySecondGuitar("Gibson", "Les Paul", "Electric");
+        guitar2.pluggingGuitarCable();
+        guitar2.pluggingToAmp();
     }
 }
